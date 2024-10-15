255722a63566df2a20f0231a5160c5382e7eb075
Nattakit Namwong <mrjokit@gmail.com>
2024-10-15 09:57:30 +0700

    Random Card System

--- Lofty2024/Assets/CardSlot.cs
+++ Lofty2024/Assets/CardSlot.cs
@@ -6,14 +6,21 @@ using UnityEngine.UI;
 
 public class CardSlot : MonoBehaviour
 {
+    public int cardIndex;
     public Image cardImage;
     public TextMeshProUGUI cardName;
     public TextMeshProUGUI cardDetail;
 
-    public void SetCard(Sprite cardImage,string cardName,string cardDetail)
+    public void SetCard(int cardIndex,ArtifactData artifactData)
     {
-        this.cardImage.sprite = cardImage;
-        this.cardName.text = cardName;
-        this.cardDetail.text = cardDetail;
+        this.cardIndex = cardIndex;
+        this.cardImage.sprite = artifactData.artifactImage;
+        this.cardName.text = artifactData.artifactName;
+        this.cardDetail.text = artifactData.artifactDetail;
+    }
+
+    public void SelectedCard()
+    {
+        GameManager.Instance.GetComponent<RandomCardManager>().SelectedCard(cardIndex);
     }
 }
--- Lofty2024/Assets/RandomCardManager.cs
+++ Lofty2024/Assets/RandomCardManager.cs
@@ -1,18 +1,124 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+using VInspector;
 
 public class RandomCardManager : MonoBehaviour
 {
+    public PlayerArtifact player;
+    public GameObject cardRandomCanvas;
+    [Header("Random Card")]
+    public GameObject cardSlotPrefab;
+    public Transform cardSlotParent;
+    [Space(10)] 
+    public List<CardSlot> cardSlots;
+    [Space(10)]
     public List<ArtifactData> cardList;
     [Space(10)] 
-    [Range(1,10)]public int randomCount;
+    public List<ArtifactData> currentCardRandom;
+    [Space(10)] 
+    [Range(1,4)]public int randomCount;
     [Space(10)]
     public int randomCost;
+
+    [Space(20)] 
+    [Header("UI")] 
+    public bool artifactUsed;
+    public bool haveArtifact;
+    public Button randomAgainButton;
+    public Button randomAgainWithArtifactButton;
     
-    
+    [Button("RandomCard")]
     public void StartRandomCard()
     {
-        
+        cardRandomCanvas.SetActive(true);
+        for (int a = 0; a < randomCount; a++)
+        {
+            GameObject card = Instantiate(cardSlotPrefab, cardSlotParent);
+            card.GetComponent<CardSlot>().SetCard(a,RandomCardInList());
+            cardSlots.Add(card.GetComponent<CardSlot>());
+        }
+        UpdateButton();
+    }
+
+    private ArtifactData RandomCardInList()
+    {
+        int randomNumber = Random.Range(0, cardList.Count - 1);
+        ArtifactData cardData = cardList[randomNumber];
+        currentCardRandom.Add(cardList[randomNumber]);
+        cardList.Remove(cardList[randomNumber]);
+        return cardData;
+    }
+
+    public void RandomAgain(bool withArtifact)
+    {
+        ClearSlots();
+        if (withArtifact)
+        {
+            artifactUsed = true;
+            StartRandomCard();
+        }
+        else
+        {
+            GetComponent<GameCurrency>().DecreaseEricCoin(randomCost);
+            StartRandomCard();
+        }
+    }
+
+    public void SelectedCard(int cardIndex)
+    {
+        player.AddNewArtifact(currentCardRandom[cardIndex]);
+        currentCardRandom.Remove(currentCardRandom[cardIndex]);
+        RandomEnd();
+    }
+
+    private void ClearSlots()
+    {
+        foreach (ArtifactData card in currentCardRandom.ToList())
+        {
+            cardList.Add(card);
+            currentCardRandom.Remove(card);
+        }
+        foreach (CardSlot slot in cardSlots.ToList())
+        {
+            cardSlots.Remove(slot);
+            Destroy(slot.gameObject);
+        }
+    }
+
+    private void UpdateButton()
+    {
+        randomAgainButton.interactable = GetComponent<GameCurrency>().EricCoin >= randomCost;
+
+        if (haveArtifact)
+        {
+            if (!artifactUsed)
+            {
+                randomAgainWithArtifactButton.gameObject.SetActive(true);
+                randomAgainWithArtifactButton.interactable = true;
+            }
+            else
+            {
+                randomAgainWithArtifactButton.gameObject.SetActive(false);
+                randomAgainWithArtifactButton.interactable = false;
+            }
+        }
+        else
+        {
+            randomAgainWithArtifactButton.gameObject.SetActive(false);
+        }
+    }
+
+    private void RandomEnd()
+    {
+        if (artifactUsed)
+        {
+            artifactUsed = false;
+        }
+        ClearSlots();
+        cardRandomCanvas.SetActive(false);
     }
 }
