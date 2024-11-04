3353795c09200488581f72c4e7a090440a74da1b
thanatjojoe <thanatjojoe12@gmail.com>
2024-11-04 17:50:13 +0700

    Test Animation BG

--- Lofty2024/Assets/_Lofty/Jo/Scirpts/ImagePopUp.cs
+++ Lofty2024/Assets/_Lofty/Jo/Scirpts/ImagePopUp.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using TMPro; 
 
-public class ImagePopUp : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class ImagePopUp : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     public GameObject imageToShow; 
     public TMP_Text buttonText; 
+    private float originalFontSize;
 
     void Start()
     {
-      
         buttonText = GetComponentInChildren<TMP_Text>();
         
-      
         if (buttonText != null)
         {
-            buttonText.color = Color.black;
+            buttonText.color = Color.white;
+            originalFontSize = buttonText.fontSize; // Store the original font size
         }
     }
 
@@ -27,11 +27,10 @@ public class ImagePopUp : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
         Debug.Log("Mouse entered!");
         imageToShow.SetActive(true);
         
-      
-        
         if (buttonText != null)
         {
-            buttonText.color = Color.white; 
+            buttonText.color = Color.gray; 
+            buttonText.fontSize = originalFontSize * 1.1f; // Increase font size by 10%
         }
     }
 
@@ -42,13 +41,14 @@ public class ImagePopUp : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
         
         if (buttonText != null)
         {
-            buttonText.color = Color.black; 
+            buttonText.color = Color.white; 
+            buttonText.fontSize = originalFontSize; // Restore original font size
         }
     }
+
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("Mouse clicked!");
-      
         SoundManage.Instance.PlaySound(Sound.Effect1); 
     }
 }
