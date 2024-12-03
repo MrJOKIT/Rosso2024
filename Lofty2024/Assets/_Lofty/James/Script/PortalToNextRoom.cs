8ea4d9067f2cda42e46b49bedc35887679bb929c
Nattakit Namwong <mrjokit@gmail.com>
2024-12-03 13:53:45 +0700

    Demo

--- Lofty2024/Assets/_Lofty/James/Script/PortalToNextRoom.cs
+++ Lofty2024/Assets/_Lofty/James/Script/PortalToNextRoom.cs
@@ -114,7 +114,6 @@ public class PortalToNextRoom : InterfacePopUp<PortalToNextRoom>
         PortalManager.Instance.progressList[PortalManager.Instance.secondStageNumber - 1].SetBarType(roomTypeConnect);
         PortalManager.Instance.progressState = ProgressState.OnProgress;
         playerTrans.GetComponent<PlayerMovementGrid>().currentState = MovementState.Combat;
-        PortalManager.Instance.HidePortal();
         //TransitionAnimator animatorTwo = TransitionAnimator.Start(TransitionType.Fade,duration: 2f,invert:true,autoDestroy:true,playDelay:2f);
         //animatorTwo.onTransitionEnd.AddListener(GameManager.Instance.currentRoomPos.GetComponent<RoomManager>().StartRoom);
     }
